795934217b2026d7a2292dd54137d1cd32877cf7
David Gomez <tuemail@gmail.com>
2025-09-03 16:35:28 -0500

    chore: preparación para revisión

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-// @ts-check
+// astro.config.mjs
+
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // FIX: Le decimos explícitamente a Astro dónde está la carpeta pública.
+  publicDir: 'public', 
+  
+  integrations: [tailwind(), icon()]
 });
